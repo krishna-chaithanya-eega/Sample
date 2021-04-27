330e265ea64b0726ab98d9253b4888758eb1de2c
krishna-chaithanya-eega <krishnachaithanya133@gmail.com>
2021-04-27 13:19:30 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello ekc!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Basic react app</p>
     </div>
   );
 }
